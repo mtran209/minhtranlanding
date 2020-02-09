ed59b7680732ca8810ec307dc43d16ffdc19841b
Minh Tran <mtran@directtechnology.com>
2020-02-08 20:03:23 -0800

    WorkExp Modal

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,34 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { EXPERIENCE } from '../shared/experience';
-import { Media } from 'reactstrap';
+import { Media, Modal, Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 import { baseUrl } from '../shared/baseUrl';
 import { Fade, Stagger } from 'react-animation-components';
 
-function WorkExperience() {
-    return (
-        <div className="container d-flex slideContainer">
-            
-            <Stagger in delay={240}>
-            {EXPERIENCE.map(experience => {
-                return (
-                    <Fade in key={experience.id}>
-                    <div className="experienceCard">
-                        <Media id="expmediabody">
-                            <Media left href="#">
-                            <Media className="pl-3" id="expmedia" object src={baseUrl + experience.image} />
+class WorkExperience extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+            featuredExp: 1,
+        }
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        })
+    };
+
+    handleClick(e) {
+        this.toggleModal();
+        this.setState({
+            featuredExp: e,
+        });
+    }
+
+    render() {
+
+        const featuredWork = EXPERIENCE.filter(exp => (exp.id === this.state.featuredExp))[0];
+        console.log(featuredWork);
+
+        return (
+            <div className="container d-flex slideContainer">
+                <Stagger in delay={240}>
+                    {EXPERIENCE.map(experience => {
+                        return (
+                            <Fade in key={experience.id}>
+                                <div className="experienceCard" id={experience.id} key={experience.id} onClick={() => this.handleClick(experience.id)}>
+                                    <Media className="expmediabody">
+                                        <Media left href="#">
+                                            <Media className="pl-3" id="expmedia" object src={baseUrl + experience.image} />
+                                        </Media>
+                                        <Media style={{ fontSize: '2.5vmin' }} body>
+                                            <Media heading>{experience.company}</Media>
+                                            {experience.position}
+                                        </Media>
+                                    </Media>
+                                </div>
+                            </Fade>
+                        )
+                    })}
+                </Stagger>
+                <Modal
+                    isOpen={this.state.isOpen}
+                    toggle={this.toggleModal}
+                >
+                    <Media top>
+                        <Media object src={baseUrl + featuredWork.image} />
+                        <Media body>
+                            <Media className="text-center" heading>
+                                {featuredWork.company}
                             </Media>
-                            <Media style={{fontSize: '2.5vmin'}} body>
-                                <Media heading>{experience.company}</Media>
-                                {experience.position}
+                            <Media className="text-center" heading>
+                                {featuredWork.position}
                             </Media>
+                            <ul>
+                                <li>{featuredWork.p1}</li>
+                                <li>{featuredWork.p2}</li>
+                                <li>{featuredWork.p3}</li>
+                                <li>{featuredWork.p4}</li>
+                            </ul>
                         </Media>
-                    </div>
-                    </Fade>
-                )
-            })}
-            </Stagger>
-        </div>
-    );
+                    </Media>
+                </Modal>
+            </div>
+        );
+    }
 }
 
 export default WorkExperience;
--- src/shared/experience.js
+++ src/shared/experience.js
@@ -1,32 +1,62 @@
 export const EXPERIENCE = [
     {
+        id: 1,
         company: 'Paul the Tutors Education Centers',
         position: 'Junior Web Developer, [Present]',
         image: 'images/pttslogo.png',
+        p1: 'Front-End revision and conversion of website into React/Bootstrap',
+        p2: 'Refinement of site content and layout/content management',
+        p3: 'Maintenance and revision of backend code in PHP, debugging and documentation of fixes',
+        p4: 'Designed, refined, and optimized new and existing brand assets for deployment'
     },
     {
+        id: 2,
         company: 'Direct Technology',
         position: 'Service Desk Specialist II, [Present]',
         image: 'images/dtlogo.png',
+        p1: 'Escalation of Tier 1/2 tickets',
+        p2: 'Project Managed $150,000 AV conversion project of conference rooms into Zoom Rooms',
+        p3: 'Process creation and revision for rollout of new MSP Service Desk department',
+        p4: 'Technical contact for business units, communicated technical issues to non-technical units and translated business needs for technical teams and directed documentation/SOP generation'
     },
     {
+        id: 3,
         company: 'Delta Dental',
         position: 'Service Desk Analyst, [4 mos.]',
         image: 'images/ddlogo.png',
+        p1: 'Root-cause analysis of Desktop and network issues',
+        p2: 'Documentation inside automated ticketing system (JIRA/Atlassian)',
+        p3: 'Desktop support of Windows and Mac OS',
+        p4: 'Client-facing ticket resolution'
     },
     {
+        id: 4,
         company: 'MME Consulting',
         position: 'IT Support Specialist, [1 year]',
         image: 'images/mmelogo.png',
+        p1: 'Active Directory and O365/Azure administration',
+        p2: 'Patching and configuration of servers and networking devices',
+        p3: 'Revision of DNS/DHCP and network infrastructure issues',
+        p4: 'Desktop Support of Windows and Mac OS'
     },
     {
+        id: 5,
         company: 'Paul the Tutors Education Centers',
         position: 'Business Analyst / Technical Contact, [3 years]',
         image: 'images/pttslogo.png',
+        p1: 'Direct invoice reconciliation and analysis of P&L statements totaling over $600,000 in annual revenue',
+        p2: 'Communication of technical issues and processes to relevant stakeholders',
+        p3: 'Operations management for 3 educational centers',
+        p4: 'Documented training manuals and knowledge base articles'
     },
     {
+        id: 6,
         company: 'Pacific Medical',
         position: 'Business Administrator, [2 years]',
         image: 'images/pmlogo.png',
+        p1: 'Process creation for new fabrication lab',
+        p2: 'Integrated ERP systems for insurance billing and ordering with federal insurance billing codes',
+        p3: 'Training administrative staff in ERP systems relating to procurement and medical insurance billing',
+        p4: 'Purchasing and invoice reconciliation for 13 prosthetic clinics'
     },
 ]
